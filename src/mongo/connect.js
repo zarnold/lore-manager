e26184f39c3e70df3efd7072f7bc6d4e657038ce
DESKTOP-992OTQM\zarno <arnold.zephir@prevision.io>
2020-01-12 19:41:49 +0100

    CLEANUP -- Remove console log

--- src/mongo/connect.js
+++ src/mongo/connect.js
@@ -3,11 +3,9 @@ var mongoose = require('mongoose');
 
 async function connect() {
 
-    console.log(" Connecting to mongo db app");
+
     var mongoDB = 'mongodb://lore-app:lore-app-password@127.0.0.1/lore-app-db';
     var db = await mongoose.connect(mongoDB, { useNewUrlParser: true });
-
-    console.log(`### DATABASE : ${mongoose.STATES[mongoose.connection.readyState]}`);
 
 
 }
